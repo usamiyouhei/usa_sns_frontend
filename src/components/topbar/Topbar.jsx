f26d5caa297c465907aa567bf32cc663f917912b
youhei usami <usao9110@icloud.com>
2025-08-29 14:54:41 +0400

    8/29

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <Search />
+          <Search className='searchIcon'/>
           <input 
             type="text" 
             className="searchInput" 
@@ -18,17 +18,19 @@ export default function Topbar() {
           />
         </div>
       </div>
-      <div className="topbrRight">
-        <div className="topbarIconItem">
-          <Chat/>
-          <span className='topbarIconBadge'>1</span>
-        </div>
-        <div className="topbarIconItem"> 
-          <Notifications/>
-          <span className='topbarIconBadge'>2</span>
+      <div className="topbarRight">
+        <div className="topbarItemIcons">
+          <div className="topbarIconItem">
+            <Chat/>
+            <span className='topbarIconBadge'>1</span>
+          </div>
+          <div className="topbarIconItem"> 
+            <Notifications/>
+            <span className='topbarIconBadge'>2</span>
+          </div>
+          <img src="/assets/person/1.png" alt="" className='topbarImg'/>
         </div>
       </div>
-      <img src="/assets/person/1.png" alt="" className='topbarImg'/>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import "./Home.css";
 import Topbar from '../../components/topbar/Topbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Rightbar from '../../components/rightbar/Rightbar'
+import TimeLine from '../../components/timeline/TimeLine'
+
 
 export default function Home() {
   return (
     <>
       <Topbar/>
-      {/* Sidebar */}
-      {/* TimeLine */}
-      {/* Rightbar */}
+      <div className="homeContainer">
+        <Sidebar/>
+        <TimeLine/>
+        <Rightbar/>
+      </div>
     </>
     )
 }
