0af0319c8392f5eab057829dbf9faf198ba9f477
youhei usami <usao9110@icloud.com>
2025-08-31 11:35:34 +0400

    8/31

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import  "./TimeLine.css";
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 function TimeLine() {
   return (
     <div className='timeline'>
      <div className="timelineWrapper">
       <Share/>
-      {/* <Post/> */}
+      <Post/>
      </div>
     </div>
   )
