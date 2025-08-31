f1cd30826ed5df54299cbef7b0b850f45537326f
youhei usami <usao9110@icloud.com>
2025-08-31 07:30:18 +0400

    8/30

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -6,35 +6,37 @@ import { Analytics, Face, Gif, Image } from "@mui/icons-material";
 function share() {
   return (
     <div className='share'>
-      <div className="shareTop">
-        <img src="/assets/person/1.png" alt="" className='shareProfileImg'/>
-        <input
-          type="text"
-          className='shareInput'
-          placeholder='今何してる？'/>
-      </div>
-      <hr className="shareHr" />
-
-      <div className="shareButtons">
-        <div className="shareOptions">
-          <div className="shareOption">
-            <Image className='shareIcon'/>
-            <span className='shareOptionText'>写真</span>
-          </div>
-          <div className="shareOption">
-            <Gif className='shareIcon'/>
-            <span className='shareOptionText'>Gif</span>
-          </div>
-          <div className="shareOption">
-            <Face className='shareIcon'/>
-            <span className='shareOptionText'>気持ち</span>
-          </div>
-          <div className="shareOption">
-            <Analytics className='shareIcon'/>
-            <span className='shareOptionText'>投票</span>
-          </div>
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img src="/assets/person/1.png" alt="" className='shareProfileImg'/>
+          <input
+            type="text"
+            className='shareInput'
+            placeholder='今何してる？'/>
         </div>
-        <button className="shareButton">投稿</button>
+        <hr className="shareHr" />
+
+        <div className="shareButtons">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <Image className='shareIcon'/>
+              <span className='shareOptionText'>写真</span>
+            </div>
+            <div className="shareOption">
+              <Gif className='shareIcon'/>
+              <span className='shareOptionText'>Gif</span>
+            </div>
+            <div className="shareOption">
+              <Face className='shareIcon'/>
+              <span className='shareOptionText'>気持ち</span>
+            </div>
+            <div className="shareOption">
+              <Analytics className='shareIcon'/>
+              <span className='shareOptionText'>投票</span>
+            </div>
+          </div>
+          <button className="shareButton">投稿</button>
+        </div>
       </div>
     </div>
   )
