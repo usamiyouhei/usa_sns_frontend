25aee94cf611679d6fff4430c95304422b5cbeb2
youhei usami <usao9110@icloud.com>
2025-09-01 01:48:15 +0400

    9/1

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import "./Post.css";
+import { MoreVert } from "@mui/icons-material";
+
 
 function Post() {
   return (
     <div className='post'>
       <div className="postWrapper">
         <div className="postTop">
-          <img src="./assets/person/1.png" alt="" className='postProfile'/>
-          <span className='postUserName'>Usa Code</span>
-          <span className="postDate">5分前</span>
+          <div className="postTopLeft">
+            <img src="./assets/person/1.png" alt="" className='postProfileImg'/>
+            <span className='postUserName'>Usa Code</span>
+            <span className="postDate">5分前</span>
+          </div>
+        <div className="postTopRight">
+          <MoreVert/>
         </div>
-        <div className="postRight"></div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">SNSを自作中です</span>
+          <img src="./assets/post/1.jpeg" alt="" className='postImg'/>
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.png" alt="" className='likeIcon'/>
+            <span className="postLikeCounter">5人がいいねを押しました</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">4:コメント</span>
+          </div>
+        </div>
       </div>
     </div>
   )
