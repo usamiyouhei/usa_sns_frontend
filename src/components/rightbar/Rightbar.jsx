3518682f17e2d79885b680b29cffe39b81ec583a
youhei usami <usao9110@icloud.com>
2025-09-01 11:03:16 +0400

    9/1

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -13,11 +13,18 @@ function Rightbar() {
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
-            <div className="rightbarProfileContainer">
-              <img src="assets/person/1.jpeg" alt="" />
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/1.jpeg" alt="" className='rightbarProfileImg'/>
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUsername">sin code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/2.jpeg" alt="" className='rightbarProfileImg'/>
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">usami</span>
           </li>
         </ul>
       </div>
